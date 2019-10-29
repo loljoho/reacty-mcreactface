9305723e3d7f392fb3df40df86244f778839b467
loljoho <loljoho@users.noreply.github.com>
2019-10-29 16:01:35 -0400

    Restructure and restyle Subnav component

--- src/components/Subnav.js
+++ src/components/Subnav.js
@@ -7,19 +7,21 @@ class Subnav extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="subnav level container">
-          <div className="breadcrumb level-left is-small" aria-label="breadcrumbs">
-            <ul>
-              <li>
-                <NavLink to="/home" activeClassName="is-active">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
-              </li>
-              <li>
-                <NavLink to="/drivers" activeClassName="is-active">Drivers</NavLink>
-              </li>
-            </ul>
+        <nav className="subnav level has-background-white-bis">
+          <div className="container">
+            <div className="breadcrumb level-left is-small" aria-label="breadcrumbs">
+              <ul>
+                <li>
+                  <NavLink to="/home" activeClassName="is-active">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/drivers" activeClassName="is-active">Drivers</NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
       </React.Fragment>
