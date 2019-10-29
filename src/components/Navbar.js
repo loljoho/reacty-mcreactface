31a909ef41367f577b343ffa3afd777a2385fc05
loljoho <loljoho@users.noreply.github.com>
2019-10-29 15:30:51 -0400

    Restyle and restructure Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,34 +7,32 @@ import logo from '../logo.png';
 class Navbar extends Component {
   render = () => {
     return (
-      <nav className="navbar is-fixed-top">
-        <div className="container">
-          <div className="navbar-brand">
-            <Link className="navbar-item" to="/">
-              <img src={logo} alt="Reacty McReactface" />
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+            <img src={logo} alt="Reacty McReactface" />
+          </Link>
+          <div className="navbar-burger burger" data-target="siteNavbar">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div id="siteNavbar" className="navbar-menu">
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/dashboard">
+              Dashboard
             </Link>
-            <div className="navbar-burger burger" data-target="siteNavbar">
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
           </div>
 
-          <div id="siteNavbar" className="navbar-menu">
-            <div className="navbar-start">
-              <Link className="navbar-item" to="/dashboard">
-                Dashboard
-              </Link>
-            </div>
-
-            <div className="navbar-end">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/halp">
-                Halp
-              </Link>
-            </div>
+          <div className="navbar-end">
+            <Link className="navbar-item" to="/about">
+              About
+            </Link>
+            <Link className="navbar-item" to="/halp">
+              Halp
+            </Link>
           </div>
         </div>
       </nav>
