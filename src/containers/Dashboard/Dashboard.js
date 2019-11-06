6a1a78dc262f9d3678a0ede8cb12c5f3bfa04c1e
loljoho <loljoho@users.noreply.github.com>
2019-11-06 15:35:11 -0500

    Change container components to class

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../logo.png';
 
-const Dashboard = () => (
-  <div className="App__inner">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      DASH BERLIN BOARD
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Dashy McDashface
-    </a>
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      DASH BERLIN BOARD
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Dashy McDashface
-    </a>
-  </div>
-);
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data : null
+    };
+  }
+
+  componentDidMount() {
+    fetch('../data/2019-09-03_2019-10-07/channels.json')
+      .then(response => {
+        console.info(response.json());
+      }).then(data => {
+        this.setState({ data });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App__inner">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          DASH BERLIN BOARD
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dashy McDashface
+        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          DASH BERLIN BOARD
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dashy McDashface
+        </a>
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../logo.png';
 
-const Home = () => (
-  <div className="App__inner">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </div>
-);
+class Home extends Component {
+  render() {
+    return (
+      <div className="App__inner">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    );
+  }
+}
 
 export default Home;
